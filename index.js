bf39860b1828d0fd170d64fe38e0465dd0ac3be4
devniel93 <danielolanouburga@gmail.com>
2021-06-01 12:37:45 -0500

    refactorizacion de codigo response y json response para post y get

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ exports.myDemoFunction = (req, res) => {
     try {
         switch (req.path) {
             case "/":
-                res.status(200).send('Hello World!');
+                res.status(200).send({ msg: "Hello World!" });
                 break;
 
             case "/customers":
@@ -20,83 +20,97 @@ exports.myDemoFunction = (req, res) => {
                 break;
 
             default:
-                res.status(404).send("Ruta no encontrada");
+                res.status(404).send({ msg: "Ruta no encontrada" });
         }
     } catch (err) {
-        res.status(500).send(`Error: ${err.message}`);
+        res.status(500).send({ msg: `${err.message}` });
     }
 };
 
 const customerController = (req, res) => {
-    if (req.method === 'GET') {
-        getCustomer(req, res);
-    } 
-    else if (req.method === 'POST') {
-        res.status(201).send('Creating customer...');
-    } 
-    else if (req.method === 'PUT') {
-        res.status(200).send('Updating customer...');
-    } 
-    else if (req.method === 'DELETE') {
-        res.status(200).send('Removing customer...');
+    switch (req.method) {
+        case "GET":
+            getCustomer(req, res); break;
+
+        case "POST":
+            createCustomer(req, res); break;
+
+        case "PUT":
+            res.status(200).send({ msg: "Actualizando customer" }); break;
+
+        case "DELETE":
+            res.status(200).send({ msg: "Eliminando customer" }); break;
+
+        default:
+            res.status(200).send({ msg: "Alguna otra operacion" });
     }
 };
 
 const getCustomer = (req, res) => {
-    jsonArray = [];    
+    jsonArray = [];
     let statement = queries.getCustomerById;
 
     if (!req.query.customerId) {
         statement = queries.getAllCustomers;
     }
-    
-    executeStatement(statement, req.query.customerId, res);
+
+    executeStatement(statement, req.query.customerId, null, res);
 };
 
 const createCustomer = (req, res) => {
+    jsonArray = [];
     let statement = `INSERT INTO SalesLT.Customer(FirstName, LastName, CompanyName, Phone, PasswordHash, PasswordSalt) 
                     OUTPUT INSERTED.CustomerID 
-                    VALUES (@firstName, @lastName, @company, @phone, NEWID(), '')`;
-    const data = {
+                    VALUES (@firstName, @lastName, @companyName, @phone, NEWID(), '')`;
+    const dummyData = {
         customerId: 10,
         firstName: "Daniel",
         lastName: "Olano",
         companyName: "Pandero",
         phone: "966370312"
     };
+    executeStatement(statement, null, dummyData, res);
 };
 
-const executeStatement = (statement, customerId, res) => {
+const executeStatement = (statement, customerId, data, res) => {
     connection = new Connection(config);
     connection.on("connect", err => {
         if (err) {
             console.error(err);
-            res.status(500).send(`Error: ${err.message}`);
+            res.status(500).send({ msg: `${err.message}` });
         } else {
-            execStmtRequest(statement, customerId, res);
+            execStmtRequest(statement, customerId, data, res);
         }
     });
     connection.connect();
 };
 
-const execStmtRequest = (stmt, customerId, res) => {
+const execStmtRequest = (stmt, customerId, data, res) => {
+
     const request = new Request(stmt, (err, rowCount) => {
         if (err) {
             console.error(err);
-            res.status(500).send(`Error: ${err.message}`);
+            res.status(500).send({ msg: `${err.message}` });
         } else {
-            //console.log(`${rowCount} row(s) returned`);
             if (rowCount == 0) {
-                res.status(200).send("No se encontro customers");
+                res.status(200).send({ msg: "No se encontro customers" });
             } else {
-                res.status(200).json(jsonArray);
+                res.status(data ? 201 : 200).json(jsonArray);
             }
             connection.close();
         }
     });
 
+
     if (customerId) {
         request.addParameter('customerId', TYPES.Int, customerId);
+    }
+
+    if (data) {
+        request.addParameter('firstName', TYPES.NVarChar, data.firstName);
+        request.addParameter('lastName', TYPES.NVarChar, data.lastName);
+        request.addParameter('companyName', TYPES.NVarChar, data.companyName);
+        request.addParameter('phone', TYPES.NVarChar, data.phone);
     }
 
     request.on("row", columns => {
